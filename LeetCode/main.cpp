16c59e6221f10f52d5603bd483a054fb91cd3c8c
yc1258 <yancong1990@msn.com>
2015-12-24 00:21:56 +0800

    Initial Commit

---
+++ LeetCode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LeetCode
+//
+//  Created by YanCong on 15/12/24.
+//  Copyright © 2015年 YanCong. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

c5f7f19caed4d10659d336ae85a32d91219513b2
yc1258 <yancong1990@msn.com>
2015-12-25 09:36:40 +0800

    my first time to LeetCode

---
+++ LeetCode/Minimum_Depth_of_Binary_Tree.cpp
@@ -0,0 +1,9 @@
+//
+//  Minimum_Depth_of_Binary_Tree.cpp
+//  LeetCode
+//
+//  Created by YanCong on 15/12/24.
+//  Copyright © 2015年 YanCong. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ LeetCode/SameTree.cpp
@@ -0,0 +1,9 @@
+//
+//  SameTree.cpp
+//  LeetCode
+//
+//  Created by YanCong on 15/12/24.
+//  Copyright © 2015年 YanCong. All rights reserved.
+//
+
+#include <stdio.h>
